d0073fa32ba5d568159d01064378a0d784ddf34a
DmitryNestor <dimchik.livethe5@gmail.com>
2024-05-07 00:06:55 +0600

    Initial commit

---
+++ mainApp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'mainApp/homePage.html')
+
+def contact(request):
+    return render(request, 'mainApp/basic.html', {'values': ['Если у вас есть вопросы звоните по телефону', '990-015']})
